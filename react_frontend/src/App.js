d057c368c1ed992a12eb5522d06ac3a746dbcb63
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2023-05-21 17:21:22 -0500

    flashcard functionality

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,8 +1,59 @@
+import React, { useEffect, useState } from "react";
+import FlashcardList from "./FlashcardList";
+import axios from 'axios';
 
 function App() {
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
+useEffect(() => {
+    axios
+    .get('https://opentdb.com/api.php?amount=10')
+    .then(res => {
+      setFlashcards(res.data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const options = 
+        [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: options.sort(() => Math.random() - .5)
+        }
+      }))
+    })
+}, [])
+
+function decodeString(str){
+  const textArea = document.createElement('textarea')
+  textArea.innerHTML = str
+  return textArea.value
+}
+
   return (
-    <h1>Hello World</h1>
+    <div className = "container">
+    <FlashcardList flashcards = {flashcards} />
+    </div>
   );
 }
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'What is 2 + 2?',
+    answer: '4',
+    options: ['2', '3', '4', '5']
+  },
+  {
+    id: 2,
+    question: 'What is 2 + 3?',
+    answer: '5',
+    options: ['2', '3', '4', '5']
+  },
+  {
+    id: 3,
+    question: 'What is 2 + 1?',
+    answer: '3',
+    options: ['2', '3', '4', '5']
+  }
+]
 
 export default App;
---
+++ react_frontend/src/Flashcard.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from 'react'
+import './App.css'
+
+export default function Flashcard( { flashcard}) {
+    // Look up wtf a useState is
+    // Look up what React hook is
+    const [flip, setFlip] = useState(false)
+    const[height, setHeight] = useState('initial')
+    const frontEl = useRef()
+    const backEl = useRef()
+
+    function setMaxHeight() {
+        const frontHeight = frontEl.current.getBoundingClientRect().height
+        const backHeight = backEl.current.getBoundingClientRect().height
+        setHeight(Math.max(frontHeight, backHeight, 100))
+    }
+
+    useEffect(setMaxHeight, [flashcard.question, flashcard.answer, flashcard.options])
+    useEffect(() => {
+        window.addEventListener('resize', setMaxHeight)
+        return () => window.removeEventListener('resize', setMaxHeight)
+    }, [])
+
+    return (
+    <div 
+        className= {`card ${flip ? 'flip' : ''}`}
+        style = {{height: height}}
+        onClick = {() => setFlip(!flip)}
+    > 
+        <div className = "front" ref={frontEl}>
+            {flashcard.question}
+            <div className = "flashcard-options">
+                {flashcard.options.map(option => {
+                    return <div className = "flashcard-option">{option}</div>
+                })}
+            </div>
+        </div>
+        <div className = "back" ref = {backEl}>{flashcard.answer}</div>
+    </div>
+  )
+}
