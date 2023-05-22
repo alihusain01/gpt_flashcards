3e21ab3014faf752c2b7e00ac71a12371e67579c
alihusain01 <48800789+alihusain01@users.noreply.github.com>
2023-05-21 23:03:35 -0500

    added button to switch to different page

--- react_frontend/src/App.js
+++ react_frontend/src/App.js
@@ -1,59 +1,73 @@
 import React, { useEffect, useState } from "react";
 import FlashcardList from "./FlashcardList";
-import axios from 'axios';
+import axios from "axios";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
-useEffect(() => {
-    axios
-    .get('https://opentdb.com/api.php?amount=10')
-    .then(res => {
-      setFlashcards(res.data.results.map((questionItem, index) => {
-        const answer = decodeString(questionItem.correct_answer)
-        const options = 
-        [...questionItem.incorrect_answers.map(a => decodeString(a)), answer]
-        return {
-          id: `${index}-${Date.now()}`,
-          question: decodeString(questionItem.question),
-          answer: answer,
-          options: options.sort(() => Math.random() - .5)
-        }
-      }))
-    })
-}, [])
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  useEffect(() => {
+    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+      setFlashcards(
+        res.data.results.map((questionItem, index) => {
+          const answer = decodeString(questionItem.correct_answer);
+          const options = [
+            ...questionItem.incorrect_answers.map((a) => decodeString(a)),
+            answer,
+          ];
+          return {
+            id: `${index}-${Date.now()}`,
+            question: decodeString(questionItem.question),
+            answer: answer,
+            options: options.sort(() => Math.random() - 0.5),
+          };
+        })
+      );
+    });
+  }, []);
 
-function decodeString(str){
-  const textArea = document.createElement('textarea')
-  textArea.innerHTML = str
-  return textArea.value
-}
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
 
   return (
-    <div className = "container">
-    <FlashcardList flashcards = {flashcards} />
-    </div>
+    <>
+      <div className="nav-bar">
+        <div className="titles">
+          <h1 className="title">GPT-Flashcards</h1>
+          <h2 className="subtitle">Studying Supercharged by AI</h2>
+        </div>
+        <a href="upload.html">
+          <button> Button </button>
+        </a>
+      </div>
+
+      <div className="container">
+        <FlashcardList flashcards={flashcards} />
+      </div>
+    </>
   );
 }
 
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
-    question: 'What is 2 + 2?',
-    answer: '4',
-    options: ['2', '3', '4', '5']
+    question: "What is 2 + 2?",
+    answer: "4",
+    options: ["2", "3", "4", "5"],
   },
   {
     id: 2,
-    question: 'What is 2 + 3?',
-    answer: '5',
-    options: ['2', '3', '4', '5']
+    question: "What is 2 + 3?",
+    answer: "5",
+    options: ["2", "3", "4", "5"],
   },
   {
     id: 3,
-    question: 'What is 2 + 1?',
-    answer: '3',
-    options: ['2', '3', '4', '5']
-  }
-]
+    question: "What is 2 + 1?",
+    answer: "3",
+    options: ["2", "3", "4", "5"],
+  },
+];
 
 export default App;
